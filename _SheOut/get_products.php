e8fdb9d3a7b3bfc10ea283d95179aead5670ce90
Pauline Winter <pauline.winter0@gmail.com>
2025-03-07 10:36:52 +0100

    Delete _SheOut directory

--- _SheOut/get_products.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-header('Content-Type: application/json');
-
-$stmt = $pdo->query("SELECT id, name, CAST(price AS DECIMAL(10,2)) AS price, image, sizes FROM products");
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($products);
-?>
